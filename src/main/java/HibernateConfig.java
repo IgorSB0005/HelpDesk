2598c5c6fac7c0342345aede35ab6c030003ab66
Igor <igor.shpakov05@gmail.com>
2021-03-31 13:22:24 +0300

    Добавил User и HibernateConfig

---
+++ src/main/java/HibernateConfig.java
@@ -0,0 +1,38 @@
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+    @EnableTransactionManagement
+    @PropertySource("classpath:application.properties")
+    public class HibernateConfig {
+
+        @Autowired
+        private ApplicationContext context;
+
+        @Autowired
+        private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+        dataSource.setUrl(env.getProperty("jdbc.url"));
+        dataSource.setUsername(env.getProperty("jdbc.user"));
+        dataSource.setPassword(env.getProperty("jdbc.pass"));
+
+        return dataSource;
+    }
+
+
+
+
+}
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,21 +1,59 @@
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="USER")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID", unique = true, nullable = false)
     private int id;
-    private String login;
+
+    @Column(name ="FIRST_NAME", nullable = false)
+    private String firstName;
+
+    @Column(name ="LAST_NAME", nullable = false)
+    private String lastName;
+
+    @Column(name ="ROLE_ID", nullable = false)
+    private String role;
+
+    @Column(name ="EMAIL", unique = true, nullable = false)
+    private String email;
+
+    @Column(name ="PASSWORD", nullable = false)
     private  String password;
+
+
+    public User(){}
+
+    public User(String email) {
+        this.email = email;
+    }
 
     public int getId(){return id;}
 
     public void setId(int id){ this.id = id; }
 
-    public String getLogin(){ return  login; }
+    public String getFirstName(){return firstName;}
 
-    public  void setLogin (String login){ this.login = login; }
+    public void setFirstName(String firstName){ this.firstName = firstName; }
+
+    public String getLastName(){return lastName;}
+
+    public void setLastName(String lastName){ this.lastName = lastName; }
+
+    public String getRole(){return role;}
+
+    public void setRole(String role){ this.role = role; }
+
+    public String getEmail(){ return  email; }
+
+    public  void setEmail (String login){ this.email = email; }
 
     public String getPassword(){ return  password; }
 
     public void setPassword (String password){ this.password = password; }
-
-
-
 
 }
